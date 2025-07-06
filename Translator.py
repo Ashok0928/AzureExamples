b6e05ed11bf729d3b689d8af35e8bb00f0afc523
Ashok0928 <ashokkumark.k@outlook.com>
2025-07-06 11:06:36 -0400

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,36 @@
+import requests, uuid, json
+
+# Add your key and endpoint
+key = "HbHW0VI0unUi1lYOd7LFmiTxKVSfsiUSgaqz72cDMtX1GO6onrWJJQQJ99BGACYeBjFXJ3w3AAAbACOGkab5"
+endpoint = "https://api.cognitive.microsofttranslator.com/"
+
+# location, also known as region.
+# required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+location = "eastus"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['fr', 'zu']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    # location required if you're using a multi-service or regional (not global) resource.
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text': 'I would really like to go road trip with Rishika!'
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
